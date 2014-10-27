0e64923af9803ee304c09d76dce0ff5a763de427
Cory Johns <johnsca@gmail.com>
2014-10-27 18:19:55 -0400

    Improve unit selection, bug fixes, and refactors

--- jdhe-init.sh
+++ jdhe-init.sh
@@ -6,11 +6,14 @@ fi
 
 if [[ -e "$JDHE_USER_INIT" ]]; then
     if [[ -d "$JDHE_USER_INIT" ]]; then
-        for script in "$(ls $JDHE_USER_INIT)"; do
-            $JDHE_USER_INIT/$script
-        done
+        init_files="$(ls $JDHE_USER_INIT)"
+        if [[ -n "$init_files" ]]; then
+            for script in "$init_files"; do
+                $JDHE_USER_INIT/$script || exit $?
+            done
+        fi
     else
-        ./$JDHE_USER_INIT
+        ./$JDHE_USER_INIT || exit $?
     fi
     rm -rf "$JDHE_USER_INIT"
 fi
